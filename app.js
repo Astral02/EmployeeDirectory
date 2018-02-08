e0db0dcb3271b9d33fd8f437aa9849018c4b2c83
Vikas <vikas04522@gmail.com>
2018-02-08 02:06:13 -0500

    removing the host name

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var chalk = require('chalk');
 var routes = require('./routes');
 
 app.use('/', routes);
-app.listen(config.port, config.host, function(){
-    console.log(chalk.green('Server started at ' + config.host +':'+ config.port));
+app.listen(config.port, function(){
+    console.log(chalk.green('Server started at port' + config.port));
 })
 
